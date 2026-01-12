243da485b3986d864ada8187d5f45ae62d2c2d84
aduribhavishya <aduribhavishya2@gmail.com>
2026-01-12 23:28:22 +0530

    Day-2 programs uploded

---
+++ Day-2/operators.c
@@ -0,0 +1,53 @@
+operators are special symbols used in c-programming to perform operations on variables and values
+Types of operators
+1.Arthimatic operators
+it is used for mathematical calculations
+>+ Addition->a+b
+>- Subtraction->a-b
+>* Multiplication->a*b
+>/ Division->a/b
+% Modulus(remainder)->a%b
+2.Assignment Operators
+The assignment operators is = it is used to store rightside value of the left side variables
+a=6;its means that the value 6 stores in 'a'
+>Compound assignment operators: there are five compound assignment opperators in 'c' program they are
++=,-=,*=,\=,%=
+3.Logical operators 
+used to combine conditions
+1.&&-logical AND
+2.||-logical OR
+3.!-logical NOT
+4.Relational operators
+used to compare two values
+== Equal to
+!=Not equal to
+> grater than 
+< less than
+>=greater than or equal to
+<=less than or equal to
+[if true means the value is 1]
+[if false means the value is 0]
+5.Increment and decrement operators
+used to incraese or decrease value by 1
+>> ++ Increment 
+>> --Decrement
+they are types of
+Pre-increment ++a
+Post-increment a++
+Pre-Decrement a--
+Post-Decrement --a
+6.Bitwise opperators
+opperator on binary values
+&AND
+|OR
+^XOR
+-NOT
+<<left shift
+>>Right shift
+7.Conditional operator
+if-else statement
+8.Special operators
+sizeof C-gives size of data type
+typeof javascript-gives data type
+,comma opperator
+these are the operators
